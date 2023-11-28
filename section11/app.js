afc2f5b8618225ad174d31c0828abb60d6fc83a1
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2023-11-28 19:12:24 +0900

    자바 스크립트 코드를 외부 파일로 아웃소싱 [30일차]

---
+++ section11/app.js
@@ -0,0 +1,10 @@
+let greetingText = "Hello I'm Son MinSeock!";
+let userAge = 25;
+
+alert(greetingText);
+alert(greetingText);
+
+greetingText = "Hello I'm Max!";
+alert(greetingText);
+
+alert(userAge);
