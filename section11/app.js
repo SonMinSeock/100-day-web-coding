78c6a7da5ce2063febaf6d2aa5627f2d9ca6286c
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2023-11-28 19:48:09 +0900

    객체 소개(관련 데이터 그룹화) [30일차]

--- section11/app.js
+++ section11/app.js
@@ -2,4 +2,6 @@ let userName = "Son MinSeock";
 let userAge = 25;
 let hobbies = ["Socker", "Cooking", "Music"];
 
+let job = { title: "Developer", place: "Seoul", salary: 52000 };
+
 alert(hobbies[1]);
