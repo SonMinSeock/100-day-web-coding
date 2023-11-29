19f36021df15838369a0da682c1d34ee1b13b67f
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2023-11-29 19:37:39 +0900

    매개변수를 사용하여 함수에 데이터 전달 [31일차]

--- section11/app.js
+++ section11/app.js
@@ -5,14 +5,14 @@ let hobbies = ["Socker", "Cooking", "Music"];
 let job = { title: "Developer", place: "Seoul", salary: 52000 };
 
 let totalAdultYears;
-function calculateAdultYears() {
+function calculateAdultYears(userAge) {
   return userAge - 18;
 }
 
-totalAdultYears = calculateAdultYears();
+totalAdultYears = calculateAdultYears(userAge);
 alert(totalAdultYears);
 
 userAge = 35;
-totalAdultYears = calculateAdultYears();
+totalAdultYears = calculateAdultYears(userAge);
 
 alert(totalAdultYears);
