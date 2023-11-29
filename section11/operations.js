9c5b34ffba0a706a3c7c5dea359a904be423ba96
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2023-11-30 08:13:16 +0900

    수학 연산 & 수학 규칙 [32일차]

---
+++ section11/operations.js
@@ -0,0 +1,19 @@
+console.log(8 + 2);
+console.log(8 - 2);
+console.log(8 * 2);
+console.log(10 / 4);
+console.log(10 % 4);
+
+console.log(10 + (3 - 5) * 10);
+
+let result = (10 + 3 - 5) * 10;
+
+//result = result + 1;
+//result++;
+//result--;
+// console.log(result);
+result += 5;
+result -= 5;
+result /= 5;
+result *= 5;
+console.log(result);
