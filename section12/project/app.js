45ced773d189ae466d47dc598c4e65b5853b512f
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2023-12-02 13:11:51 +0900

    DOM에서 요소 검색하기 [34일차]

--- section12/project/app.js
+++ section12/project/app.js
@@ -1,1 +1,6 @@
-document.body.children[1].children[0].href = "https://google.com";
+// DOM으로 요소를 접근 하는방법. 1) 드릴링 방법.
+//document.body.children[1].children[0].href = "https://google.com";
+
+// DOM으로 요소를 접근 하는방법. 2) 쿼리 메서스 방법.
+let aElement = document.getElementById("external-link");
+aElement.href = "https://google.com";
