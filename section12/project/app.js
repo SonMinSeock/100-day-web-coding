a7c0119abf9b61dbc05617ba126caaba0c452263
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2023-12-03 17:45:39 +0900

    사용자 입력 이벤트 듣기 [35일차]

--- section12/project/app.js
+++ section12/project/app.js
@@ -1,7 +1,14 @@
 let paragrapElement = document.querySelector("p");
+let inputElement = document.querySelector("input");
 
 function changePragraphText() {
   paragrapElement.textContent = "Clicked!";
   console.log("paragraph clicked!!");
 }
+function enteredTextHandler() {
+  let enteredText = inputElement.value;
+  console.dir(enteredText);
+}
+
 paragrapElement.addEventListener("click", changePragraphText);
+inputElement.addEventListener("input", enteredTextHandler);
