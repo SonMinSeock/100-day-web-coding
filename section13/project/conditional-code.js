ff1068c78585fab370b6ef6672901bf5107cb4b1
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2023-12-07 20:42:20 +0900

    부울 값에 대해 자세히 알아보기 [38일차]

--- section13/project/conditional-code.js
+++ section13/project/conditional-code.js
@@ -3,3 +3,21 @@ const myName = "Max";
 if (true) {
   console.log("Hello!");
 }
+
+let isLoggedIn = false;
+
+// if (isLoggedIn === true) {
+//   console.log("User is Logged In!");
+// }
+
+// if (isLoggedIn) {
+//   console.log("User is Logged In!");
+// }
+
+// if (isLoggedIn === false) {
+//   console.log("User is NOT Logged In!");
+// }
+
+if (!isLoggedIn) {
+  console.log("User is NOT Logged In!");
+}
