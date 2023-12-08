ee5913e49f405d895a1ca3be8dc2598361994035
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2023-12-08 18:34:55 +0900

    연습 시간: 설정 & "for" 반복문 [39일차]

--- section13/roop-pratice/app.js
+++ section13/roop-pratice/app.js
@@ -1,29 +1,19 @@
-for (let i = 0; i < 10; i++) {
-  console.log(i);
+const calculatedBtn = document.querySelector("#calculator button");
+
+function calculatedSum() {
+  const userNumberInput = document.getElementById("user-number");
+  const calculatedSumParagraph = document.getElementById("calculated-sum");
+
+  const enteredNumber = userNumberInput.value;
+
+  let sum = 0;
+
+  for (let i = 0; i <= enteredNumber; i++) {
+    sum += i;
+  }
+
+  calculatedSumParagraph.textContent = sum;
+  calculatedSumParagraph.style.display = "block";
 }
 
-const users = ["Son", "Lee", "Kim"];
-
-for (const userName of users) {
-  console.log(userName);
-}
-
-// for (let i = 0; i < users.length; i++) {
-//   console.log(users[i]);
-// }
-
-const loggedInUser = {
-  name: "Son MinSeock",
-  age: 25,
-  isAdmin: true,
-};
-
-for (const propertyName in loggedInUser) {
-  console.log(loggedInUser[propertyName]);
-}
-
-let isFinished = false;
-while (!isFinished) {
-  isFinished = confirm("Do you want to quit?");
-}
-console.log("Done!");
+calculatedBtn.addEventListener("click", calculatedSum);
