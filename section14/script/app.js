e35c215375a750d1d5dcb4e57f810a35b7125aa1
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2023-12-11 20:33:46 +0900

    자바스크립트 사용자 입력 확인 [42일차]

--- section14/script/app.js
+++ section14/script/app.js
@@ -4,6 +4,7 @@ const cancleConfigBtnElement = document.getElementById("cancle-config-btn");
 const editPlayer1BtnElement = document.getElementById("edit-player-1-btn");
 const editPlayer2BtnElement = document.getElementById("edit-player-2-btn");
 const formElement = document.querySelector("form");
+const errorOutputElement = document.getElementById("config-errors");
 
 editPlayer1BtnElement.addEventListener("click", openPlayerConfig);
 editPlayer2BtnElement.addEventListener("click", openPlayerConfig);
--- section14/script/config.js
+++ section14/script/config.js
@@ -6,11 +6,19 @@ function openPlayerConfig() {
 function closePlayerConfig() {
   backdropElement.style.display = "none";
   playerConfigOverlayElement.style.display = "none";
+  formElement.firstElementChild.classList.remove("error");
+  errorOutputElement.textContent = "";
 }
 
 function savePlayerConfig(event) {
   event.preventDefault();
   const formData = new FormData(event.target);
-  const enteredPlayerName = formData.get("playername");
-  console.log(enteredPlayerName);
+  const enteredPlayerName = formData.get("playername").trim();
+
+  if (!enteredPlayerName) {
+    // event.target.firstElementChild.className = "error";
+    event.target.firstElementChild.classList.add("error");
+    errorOutputElement.textContent = "Please enter a valid name!";
+    return;
+  }
 }
