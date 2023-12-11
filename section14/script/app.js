7d2981da3bbb6220e215490aae98f3a7bbf336f0
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2023-12-11 20:14:33 +0900

    자바 스크립트로 양식 제출 처리(FormData) [42일차]

--- section14/script/app.js
+++ section14/script/app.js
@@ -3,8 +3,10 @@ const playerConfigOverlayElement = document.getElementById("config-overlay");
 const cancleConfigBtnElement = document.getElementById("cancle-config-btn");
 const editPlayer1BtnElement = document.getElementById("edit-player-1-btn");
 const editPlayer2BtnElement = document.getElementById("edit-player-2-btn");
+const formElement = document.querySelector("form");
 
 editPlayer1BtnElement.addEventListener("click", openPlayerConfig);
 editPlayer2BtnElement.addEventListener("click", openPlayerConfig);
 cancleConfigBtnElement.addEventListener("click", closePlayerConfig);
 backdropElement.addEventListener("click", closePlayerConfig);
+formElement.addEventListener("submit", savePlayerConfig);
--- section14/script/config.js
+++ section14/script/config.js
@@ -7,3 +7,10 @@ function closePlayerConfig() {
   backdropElement.style.display = "none";
   playerConfigOverlayElement.style.display = "none";
 }
+
+function savePlayerConfig(event) {
+  event.preventDefault();
+  const formData = new FormData(event.target);
+  const enteredPlayerName = formData.get("playername");
+  console.log(enteredPlayerName);
+}
