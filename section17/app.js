3789dcd4536dca4b89f623def3c12fcb44031e97
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2023-12-17 18:42:30 +0900

    익스프레스로 사용자 데이터 구문 분석 [48일차]

--- section17/app.js
+++ section17/app.js
@@ -2,12 +2,21 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.urlencoded({ extended: false }));
+
 app.get("/currenttime", function (req, res) {
   res.send("<h1>" + new Date().toISOString() + "</h1>");
 }); // localhost:3000/currenttime
 
 app.get("/", function (req, res) {
-  res.send("<h1>Hello World!</h1>");
+  res.send(
+    "<form action='/store-user' method='POST'><label for='username'>Your Name</label><input type='text' id='username' name='username'/><button>Submit</button></form>"
+  );
 }); // localhost:3000/
+
+app.post("/store-user", function (req, res) {
+  const username = req.body.username;
+  res.send("<h1>" + username + "</h1>");
+});
 
 app.listen(3000);
