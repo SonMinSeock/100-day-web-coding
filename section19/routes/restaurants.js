7723d0264210d8c3cebdd1d47bfbd77cb3cdffc1
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2023-12-23 15:47:05 +0900

    쿼리 매개변수(& 숨겨진 양식 필드) 소개 [53일차]

--- section19/routes/restaurants.js
+++ section19/routes/restaurants.js
@@ -4,9 +4,31 @@ const resData = require("../util/restaurant-data");
 const uuid = require("uuid");
 
 router.get("/restaurants", function (req, res) {
+  let order = req.query.order;
+  let nextOrder = "desc";
+
   const storedRestaurants = resData.getStoredRestaurants();
 
-  res.render("restaurants", { numberOfRestaurants: storedRestaurants.length, restaurants: storedRestaurants });
+  if (order !== "asc" && order !== "desc") {
+    order = "asc";
+  }
+
+  if (order === "desc") {
+    nextOrder = "asc";
+  }
+
+  storedRestaurants.sort(function (resA, resB) {
+    if ((order === "asc" && resA.name > resB.name) || (order === "desc" && resB.name > resA.name)) {
+      return 1;
+    } else {
+      return -1;
+    }
+  });
+  res.render("restaurants", {
+    numberOfRestaurants: storedRestaurants.length,
+    restaurants: storedRestaurants,
+    nextOrder: nextOrder,
+  });
 });
 
 router.get("/restaurants:id", function (req, res) {
