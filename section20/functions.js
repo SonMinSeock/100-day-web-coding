e708a72398d0151448d976d407ca38b90e982d0f
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2023-12-24 19:41:57 +0900

    나머지 매개변수 & 스프레드 연산자 [54일차]

--- section20/functions.js
+++ section20/functions.js
@@ -4,3 +4,15 @@ function greetUser(greetingPrefix, userName = "User") {
 
 greetUser("Hi", "Son MinSeock");
 greetUser("Hello");
+
+function sumUp(...numbers) {
+  let result = 0;
+  for (const number of numbers) {
+    result += number;
+  }
+
+  return result;
+}
+
+const inputNumbers = [1, 4, 10, 16];
+console.log(sumUp(...inputNumbers));
