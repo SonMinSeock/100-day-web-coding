f2260c25aa5edfe05104e598ab50cf5877e12090
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2023-12-30 08:58:17 +0900

    프로미스 소개 [56일차]

--- section20/async.js
+++ section20/async.js
@@ -1,12 +1,21 @@
 // 비동기 처리 이해.
-const fs = require("fs");
+const fs = require("fs/promises");
 
 function readFile() {
   // 동기 파일 읽기.
   //const fileData = fs.readFileSync("data.txt");
 
   // 비동기 파일 읽기.
+  /*
   fs.readFile("data.txt", function (error, fileData) {
+    console.log("File parsing Done!");
+    console.log(fileData.toString());
+    // another async work...
+  });
+  */
+
+  // 프로미스
+  fs.readFile("data.txt").then(function (fileData) {
     console.log("File parsing Done!");
     console.log(fileData.toString());
   });
