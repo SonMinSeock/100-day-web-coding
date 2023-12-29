0e77b4db19de1f07bff0b31bf23a796ef01372ba
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2023-12-29 15:53:59 +0900

    가변 범위 지정(Block Scope) & 섀도잉 [55일차]

--- section20/errors.js
+++ section20/errors.js
@@ -2,11 +2,15 @@
 const fs = require("fs");
 
 function readFile() {
+  let fileData;
   try {
-    const fileData = fs.readFileSync("data.json");
+    fileData = fs.readFileSync("data.json");
+    // 새도잉
+    //const fileData = fs.readFileSync("data.json");
   } catch (error) {
     console.log(`An error occurred!`);
   }
+  console.log(fileData);
   console.log("Hello there!");
 }
 
