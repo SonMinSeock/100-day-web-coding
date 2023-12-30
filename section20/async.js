0a054b119721e7156a1d7ffd91669c250a7fe57d
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2023-12-30 09:23:06 +0900

    async / await로 코드 개선 [56일차]

--- section20/async.js
+++ section20/async.js
@@ -1,28 +1,16 @@
 // 비동기 처리 이해.
 const fs = require("fs/promises");
 
-function readFile() {
-  // 동기 파일 읽기.
-  //const fileData = fs.readFileSync("data.txt");
-
-  // 비동기 파일 읽기.
-  /*
-  fs.readFile("data.txt", function (error, fileData) {
-    console.log("File parsing Done!");
-    console.log(fileData.toString());
-    // another async work...
-  });
-  */
-
-  // 프로미스
-  fs.readFile("dat.txt")
-    .then(function (fileData) {
-      console.log("File parsing Done!");
-      console.log(fileData.toString());
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
+async function readFile() {
+  let fileData;
+  try {
+    fileData = (await fs.readFile("data.txt")).toString();
+    //fileData = (await fs.readFile("dat.txt")).toString();
+  } catch (error) {
+    console.log(error);
+  }
+  console.log("File parsing Done!");
+  console.log(fileData);
 
   console.log("Hello there!");
 }
