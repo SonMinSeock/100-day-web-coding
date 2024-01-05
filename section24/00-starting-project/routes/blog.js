1e5165ab2e24594795cc8ea200285e6351a8345b
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2024-01-05 23:16:38 +0900

    단일 블로그 게시물 가져오기 & 표시 [62일차]

--- section24/00-starting-project/routes/blog.js
+++ section24/00-starting-project/routes/blog.js
@@ -30,4 +30,22 @@ router.post("/posts", async function (req, res) {
   res.redirect("/posts");
 });
 
+router.get("/posts/:id", async function (req, res) {
+  const { id } = req.params;
+
+  const query = `
+    SELECT posts.*, authors.name AS author_name, authors.email AS author_email FROM POSTS
+    INNER JOIN authors ON posts.author_id = authors.id
+    WHERE posts.id = ?
+  `;
+
+  const [posts] = await db.query(query, [id]);
+
+  if (!posts || posts.length === 0) {
+    return res.status(404).render("404");
+  }
+
+  res.render("post-detail", { post: posts[0] });
+});
+
 module.exports = router;
