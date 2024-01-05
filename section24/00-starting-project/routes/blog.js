13c645030604bb15e66cb2ca0665283ece894699
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2024-01-06 00:07:14 +0900

    게시물 업데이트 [62일차]

--- section24/00-starting-project/routes/blog.js
+++ section24/00-starting-project/routes/blog.js
@@ -74,4 +74,18 @@ router.get("/posts/:id/edit", async function (req, res) {
   res.render("update-post", { post: posts[0] });
 });
 
+router.post("/posts/:id/edit", async function (req, res) {
+  const { title, summary, content } = req.body;
+  const { id } = req.params;
+
+  const query = `
+    UPDATE posts SET title = ?, summary = ?, body = ?
+    WHERE id = ?
+  `;
+
+  await db.query(query, [title, summary, content, id]);
+
+  res.redirect("/posts");
+});
+
 module.exports = router;
