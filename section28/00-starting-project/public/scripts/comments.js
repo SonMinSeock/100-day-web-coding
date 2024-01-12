434a7ac09b4fe78a24ef8bb3ecbfafb7dbc86a84
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2024-01-12 21:51:13 +0900

    POST 요청 데이터 준비 [69일차]

--- section28/00-starting-project/public/scripts/comments.js
+++ section28/00-starting-project/public/scripts/comments.js
@@ -1,5 +1,8 @@
 const loadCommentsBtnElement = document.getElementById("load-comments-btn");
 const commentsSectionElement = document.getElementById("comments");
+const commentsFormElement = document.querySelector("#comments-form form");
+const commentTitleElement = document.getElementById("title");
+const commentTextElement = document.getElementById("text");
 
 function createCommentsList(comments) {
   const commentListElement = document.createElement("ol");
@@ -30,4 +33,13 @@ async function fetchCommentsForPost(event) {
   commentsSectionElement.appendChild(commentListElement);
 }
 
+async function saveComment(event) {
+  event.preventDefault();
+
+  const enteredTitle = commentTitleElement.value;
+  const enteredText = commentTextElement.value;
+  console.log(enteredText, enteredTitle);
+}
+
 loadCommentsBtnElement.addEventListener("click", fetchCommentsForPost);
+commentsFormElement.addEventListener("submit", saveComment);
