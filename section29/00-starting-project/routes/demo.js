f2d08037796de35667fb4f0289c63e9a8eb90620
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2024-01-14 10:22:58 +0900

    사용자 로그인 기능 추가 [71일차]

--- section29/00-starting-project/routes/demo.js
+++ section29/00-starting-project/routes/demo.js
@@ -36,7 +36,28 @@ router.post("/signup", async function (req, res) {
   res.redirect("/login");
 });
 
-router.post("/login", async function (req, res) {});
+router.post("/login", async function (req, res) {
+  const userData = req.body;
+  const enteredEmail = userData.email;
+  const enteredPassword = userData.password;
+
+  const exitingEmail = await db.getDb().collection("users").findOne({ email: enteredEmail });
+
+  if (!exitingEmail) {
+    console.log("Could not log in!");
+    return res.redirect("/login");
+  }
+
+  const passwordsAreEqual = await bcrypt.compare(enteredPassword, exitingEmail.password);
+
+  if (!passwordsAreEqual) {
+    console.log("Could not log in - passwords are not equal!");
+    return res.redirect("/login");
+  }
+
+  console.log("User is authenticated!");
+  res.redirect("/admin");
+});
 
 router.get("/admin", function (req, res) {
   res.render("admin");
