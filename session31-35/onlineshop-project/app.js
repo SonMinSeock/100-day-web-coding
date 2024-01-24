f483d72949bb3d31e91aa0cc19c0026c933589f2
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2024-01-24 21:34:37 +0900

    몽고DB 데이터베이스 서버 및 연결 추가 [80일차]

--- session31-35/onlineshop-project/app.js
+++ session31-35/onlineshop-project/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const authRouter = require("./routes/auth.routes");
 const path = require("path");
+const db = require("./data/database");
 
 const PORT = 3000;
 
@@ -10,8 +11,16 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: false }));
 app.use(authRouter);
 
-app.listen(PORT, () => {
-  console.log(`localhost:${PORT} SERVER RUNING`);
-});
+db.connectToDatabase()
+  .then(function () {
+    app.listen(PORT, () => {
+      console.log(`localhost:${PORT} SERVER RUNING`);
+    });
+  })
+  .catch(function (error) {
+    console.log("Failed to connect to the database!");
+    console.log(error);
+  });
