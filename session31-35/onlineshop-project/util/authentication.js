bfe6264ed07a8df6e5301b4682c0fb797efed75f
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2024-01-27 21:18:11 +0900

    관리자 권한 부여 & 보호 탐색 [83일차]

--- session31-35/onlineshop-project/util/authentication.js
+++ session31-35/onlineshop-project/util/authentication.js
@@ -1,5 +1,6 @@
 function createUserSession(req, user, action) {
   req.session.uid = user._id.toString(); // user._id는 몽고 DB에서 받아온 유저 문서의 식별자 _id이다. ObjectId 타입에서 String 타입 변환.
+  req.session.isAdmin = user.isAdmin;
   req.session.save(action);
 }
 
