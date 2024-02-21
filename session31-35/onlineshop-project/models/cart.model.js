6f20557417fee47e23cfa6424fce592b1a0c18e1
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2024-02-21 20:23:17 +0900

    장바구니 페이지 추가 [90일차]

--- session31-35/onlineshop-project/models/cart.model.js
+++ session31-35/onlineshop-project/models/cart.model.js
@@ -14,8 +14,10 @@ class Cart {
     for (let i = 0; i < this.items.length; i++) {
       const item = this.items[i];
       if (item.product.id === product.id) {
-        cartItem.quantity += 1;
-        cartItem.totalPrice = cartItem.totalPrice + product.price;
+        cartItem.quantity = item.quantity + 1;
+        cartItem.totalPrice = item.totalPrice + product.price;
+        this.items[i] = cartItem;
+
         this.totalQunatity++;
         this.totalPrice += product.price;
         return;
