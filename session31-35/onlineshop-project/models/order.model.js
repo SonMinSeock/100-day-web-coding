09b8db733410bb14da53c8f6bf3c2cc53019140f
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2024-02-23 06:49:27 +0900

    주문 표시(고객 & 관리자용) [92일차]

--- session31-35/onlineshop-project/models/order.model.js
+++ session31-35/onlineshop-project/models/order.model.js
@@ -1,4 +1,5 @@
 const db = require("../data/database");
+const mongodb = require("mongodb");
 
 class Order {
   // status => pending, fulfilled, cancelled
@@ -17,9 +18,45 @@ class Order {
     this.id = orderId;
   }
 
+  static transformOrderDocument(orderDoc) {
+    return new Order(orderDoc.productData, orderDoc.userData, orderDoc.status, orderDoc.date, orderDoc._id);
+  }
+
+  static transformOrderDocuments(orderDocs) {
+    return orderDocs.map(this.transformOrderDocument);
+  }
+
+  static async findAll() {
+    const orders = await db.getDb().collection("orders").find().toArray();
+
+    return this.transformOrderDocuments(orders);
+  }
+
+  static async findAllForUser(userId) {
+    const uid = new mongodb.ObjectId(userId);
+
+    const orders = await db.getDb().collection("orders").find({ "userData._id": uid }).sort({ _id: -1 }).toArray();
+
+    return this.transformOrderDocuments(orders);
+  }
+
+  static async findById(orderId) {
+    const order = await db
+      .getDb()
+      .collection("orders")
+      .findOne({ _id: new mongodb.ObjectId(orderId) });
+
+    return this.transformOrderDocument(order);
+  }
+
   save() {
     if (this.id) {
       // 기존 주문에 업데이트 ...
+      const orderId = new mongodb.ObjectId(this.id);
+      return db
+        .getDb()
+        .collection("orders")
+        .updateOne({ _id: orderId }, { $set: { status: this.status } });
     } else {
       // 새로 만들어서 저장...
       const orderDocument = {
