0b548b71c093eb57d0b4f51d888b0e5e137d2e2c
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2024-02-28 11:26:03 +0900

    더 복잡한 REST API: 첫 번째 단계 [Day 96]

---
+++ section35-todo-project/more-complex-api-starting-snapshot/controllers/todos.controller.js
@@ -0,0 +1,44 @@
+const Todo = require("../models/todo.model");
+
+async function getAllTodos(req, res, next) {
+  let toDos;
+
+  try {
+    toDos = await Todo.getAllTodos();
+  } catch (error) {
+    next(error);
+  }
+
+  res.json({
+    toDos,
+  });
+}
+
+async function addTodo(req, res, next) {
+  const todoText = req.body.text;
+  const todo = new Todo(todoText);
+
+  let insertedId;
+
+  try {
+    const result = await todo.save(); // result의 객체에 insertedId 프로퍼티가 있는데 생성된 일정의 ObjectId 값이다.
+    insertedId = result.insertedId;
+  } catch (error) {
+    next(error);
+  }
+
+  todo.id = insertedId.toString();
+
+  res.json({ message: "Added todo successfully!", createdTodo: todo });
+}
+
+function updateTodo(req, res, next) {}
+
+function deleteTodo(req, res, next) {}
+
+module.exports = {
+  getAllTodos,
+  addTodo,
+  updateTodo,
+  deleteTodo,
+};
